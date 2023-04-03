6f78127fff4c89255377d5ae84e6ff20a6bd5515
Maksat <220451@astanait.edu.kz>
2023-04-03 08:26:40 +0600

    Lab 1

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int s = scanner.nextInt();
+        double[] a = new double[s];
+        System.out.println(avg(n,a));
+
+        }
+    public static int Pow(int a, int n) {//Problem 6
+        if (n == 0) {
+            return 1;
+        }
+        else if (n > 0) {
+            return a * Pow(a, n-1);
+        }
+        else {
+            return 1 / Pow(a, -n);
+        }
+    }
+    public static int factorial(int a){//Problem 4
+        if(a == 0){
+            return 1;
+        }
+        else{
+            return(a*factorial(a-1));
+        }
+    }
+
+
+    public static double avg(int n, double a[]){//Problem 2
+        Scanner scanner = new Scanner(System.in);
+        double sum = 0;
+        for(int i = 0; i<n; i++){
+            a[i] = scanner.nextInt();
+            sum +=a[i];
+        }
+        return sum/n;
+    }
+    public static boolean prime(int a){//Problem 3
+        int x = 0;
+        for(int i = 1; i<=a; i++){
+            if(a%i == 0){
+                x++;
+            }
+        }
+        if(x == 2){
+            System.out.println("It's a prime " + (a-1+1) );
+            return prime(a-1);
+        }
+
+        else if (x < 0 ){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    }
+
