42e4cd2c098b71667cc64429d0b1e548d26bc38e
Maksat <220451@astanait.edu.kz>
2023-04-04 13:09:18 +0600

    Lab1

--- src/Problem8.java
+++ src/Problem8.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Problem8 {
     public static void main(String[] args){
-
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.next();
+        System.out.println(string(line));
+    }
+    public static boolean string(String s){
+        if (s.isEmpty()) {//if string is empty it would return true
+            return true;
+        }
+        char element = s.charAt(0);
+        if (!Character.isDigit(element)) {//checks if the first element is not digit
+            return false;// return false, if no digit
+        }
+        return string(s.substring(1));//
     }
 }
+
